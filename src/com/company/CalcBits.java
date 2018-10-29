4196742e574fee018f323555cc177bf0b68bde89
Rodrigo Machado <rodrigosapiranga@hotmail.com>
2018-10-28 22:00:31 -0300

    Atualização de versão

--- src/com/company/CalcBits.java
+++ src/com/company/CalcBits.java
@@ -1,4 +1,28 @@
 package com.company;
 
 public class CalcBits {
+
+    public static char calc(char t){
+        switch ( t ) {
+            case '0':
+            case '1':
+            case '2':
+            case '3':
+            case '4':
+            case '5':
+            case '6':
+            case '7':
+            case '8':
+            case '9': t -= '0'; break;
+            case 'a':
+            case 'b':
+            case 'c':
+            case 'd':
+            case 'e':
+            case 'f': t =  String.valueOf(t - 'a' + 10).charAt(0); break;
+            default: ;
+        }
+
+        return t;
+    }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,27 +1,117 @@
 package com.company;
 
+import org.apache.commons.lang3.StringUtils;
+
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
+import java.math.BigDecimal;
+import java.math.BigInteger;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Scanner;
 
 public class Main {
 
+    /**
+     * For file "caso25a.txt"
+     *
+     * Start point is in: line 8 - column 0
+     * Final point is in: line 25 - column 17
+     * */
     public static void main(String[] args) {
-        System.out.println('f' & 8);
-        System.out.println("\n\n");
-        System.out.println(('f' - 'a' +10) & 8);
+        identifyStartAndEnd("caso50a.txt");
     }
 
+    private static void identifyStartAndEnd(String fileName){
+        Integer line = 0, startLinePosition = null, startCharPosition = null,endLinePosition = null, endCharPosition = null;
+        String[] letters;
+        Boolean upBit;
+        Boolean rightBit;
+        Boolean downBit;
+        Boolean leftBit;
 
-    private void readFile(String fileName){
+        try(Scanner sc = new Scanner(new File(fileName), Charset.forName("utf8"))){
+            Long totalLines = Files.newBufferedReader(Paths.get(fileName), Charset.forName("utf8")).lines().count() - 1;
 
-        try(BufferedReader br = Files.newBufferedReader(Paths.get(fileName),Charset.forName("utf8"))){
+            sc.nextLine();
 
-        }catch (IOException e){
+            while (sc.hasNextLine()){
+                String stringLine = sc.nextLine();
+                letters = stringLine.split(" ");
+
+                for(int i = 0; i < letters.length; i++){
+                    //Is first line?
+                    if(line == 0) {
+                        //Up bit
+                        if(hexToBin(letters[i]).charAt(0) == '0'){
+                            startLinePosition = line;
+                            startCharPosition = i;
+                        }
+                     //Is last line?
+                    }else if(line == totalLines.intValue() - 1){
+                        //Down bit
+                        if(hexToBin(letters[i]).charAt(2) == '0'){
+                            if (!line.equals(startLinePosition) && !Integer.valueOf(i).equals(startCharPosition)) {
+                                if (startCharPosition == null && startLinePosition == null) {
+                                    startLinePosition = line;
+                                    startCharPosition = i;
+                                } else {
+                                    endLinePosition = line;
+                                    endCharPosition = i;
+                                }
+                            }
+                        }
+                    }else{
+                        //Left bit
+                        if(hexToBin(letters[0]).charAt(3) == '0'){
+                            if(!line.equals(startLinePosition) && !Integer.valueOf(i).equals(startCharPosition)) {
+                                if (startCharPosition == null && startLinePosition == null) {
+                                    startLinePosition = line;
+                                    startCharPosition = i;
+                                } else {
+                                    endLinePosition = line;
+                                    endCharPosition = i;
+                                }
+                            }
+                        }
+
+                        //Right bit
+                        if(hexToBin(letters[letters.length - 1]).charAt(1) == '0') {
+                            if (!line.equals(startLinePosition) && !Integer.valueOf(i).equals(startCharPosition)) {
+                                if (startCharPosition == null && startLinePosition == null) {
+                                    startLinePosition = line;
+                                    startCharPosition = i;
+                                } else {
+                                    endLinePosition = line;
+                                    endCharPosition = i;
+                                }
+                            }
+                        }
+                    }
+
+                    if(startLinePosition != null && endLinePosition != null && startCharPosition != null && endCharPosition != null){
+                        break;
+                    }
+                }
+
+                if(startLinePosition != null && endLinePosition != null && startCharPosition != null && endCharPosition != null){
+                    break;
+                }
+                line++;
+            }
+        }catch (Exception e){
             e.printStackTrace();
         }
 
+        System.out.println("Start Line Position: " + startLinePosition + "\n");
+        System.out.println("Start Char Position: " + startCharPosition+ "\n");
+        System.out.println("End Line Position: " + endLinePosition+ "\n");
+        System.out.println("End Char Position: " + endCharPosition+ "\n");
+    }
+
+    private static String hexToBin(String hex){
+        return StringUtils.leftPad(new BigInteger(hex,16).toString(2),4,'0');
     }
 }
